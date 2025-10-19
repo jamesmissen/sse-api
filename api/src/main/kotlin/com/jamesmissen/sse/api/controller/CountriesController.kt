c055c33bb1cfe90c19432559fb635928b46f4318
James Missen <88438228+jamesmissen@users.noreply.github.com>
2025-10-19 14:23:47 +1300

    Created country data SSE endpoint

--- api/src/main/kotlin/com/jamesmissen/sse/api/controller/CountriesController.kt
+++ api/src/main/kotlin/com/jamesmissen/sse/api/controller/CountriesController.kt
@@ -3,12 +3,18 @@ package com.jamesmissen.sse.api.controller
 import com.jamesmissen.sse.api.model.Country
 import com.jamesmissen.sse.api.util.Constants.DATA_RESOURCE_LOCATION
 import com.jamesmissen.sse.api.util.extensions.readValue
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import org.springframework.core.io.ResourceLoader
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE
+import org.springframework.http.codec.ServerSentEvent
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import tools.jackson.databind.json.JsonMapper
+import java.lang.System.currentTimeMillis
 
 /**
  * A REST controller for country resources.
@@ -21,6 +27,16 @@ import tools.jackson.databind.json.JsonMapper
 @RestController
 @RequestMapping("/countries")
 class CountriesController(private final val jsonMapper: JsonMapper, private final val resources: ResourceLoader) {
+
+    companion object {
+
+        /**
+         * A constant for the period of delay (in milliseconds) between subsequent events.
+         *
+         * @author James Missen
+         */
+        private const val DELAY = 4000L // milliseconds
+    }
 
     /**
      * The list of all [Country] items.
@@ -42,4 +58,34 @@ class CountriesController(private final val jsonMapper: JsonMapper, private fina
      */
     @GetMapping("", produces = [APPLICATION_JSON_VALUE])
     final fun getCountries() = countryData
+
+    /**
+     * Streams country data as a continuous flow of Server-Sent Events.
+     *
+     * This endpoint produces an event stream, with each event including a [Country] `data` payload.
+     *
+     * @return A [Flow] of [ServerSentEvent]s, each with a [Country] `data` payload.
+     *
+     * @author James Missen
+     *
+     * @see ServerSentEvent
+     */
+    @GetMapping("/stream", produces = [TEXT_EVENT_STREAM_VALUE])
+    final fun getCountriesStream() = flow {
+        val initialId = currentTimeMillis() / DELAY + 1
+
+        generateSequence(initialId) { it + 1 }.forEach { id ->
+            // Wait for next event time if necessary
+            delay(DELAY * id - currentTimeMillis())
+
+            val country = countryData[(id % countryData.size).toInt()]
+
+            // Construct event
+            val event = ServerSentEvent.builder<Country>()
+                .data(country)
+                .build()
+
+            emit(event)
+        }
+    }
 }
