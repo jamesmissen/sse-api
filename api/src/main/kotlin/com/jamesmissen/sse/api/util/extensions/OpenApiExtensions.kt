7d3fd21b0967c9a1bed63d038de1124e9ce9cb8c
James Missen <88438228+jamesmissen@users.noreply.github.com>
2025-10-19 18:57:23 +1300

    Added custom OpenAPI default configurations

---
+++ api/src/main/kotlin/com/jamesmissen/sse/api/util/extensions/OpenApiExtensions.kt
@@ -0,0 +1,44 @@
+package com.jamesmissen.sse.api.util.extensions
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.Operation
+import io.swagger.v3.oas.models.PathItem
+import kotlin.reflect.full.functions
+
+/**
+ * Returns all path item elements defined in this API definition.
+ *
+ * Each item is a [PathItem] instance corresponding to a single API path.
+ *
+ * @receiver The [OpenAPI] instance to extract the [PathItem]s from.
+ *
+ * @return A [List] of all [PathItem]s, or an empty list if none are defined.
+ *
+ * @author James Missen
+ *
+ * @see PathItem
+ */
+val OpenAPI.pathItems
+    get() = paths
+        ?.values
+        ?.toList()
+        ?: emptyList()
+
+/**
+ * Returns all operation elements defined in this API definition.
+ *
+ * Each item is an [Operation] instance corresponding to a single API endpoint.
+ *
+ * @receiver The [OpenAPI] instance to extract the [Operation]s from.
+ *
+ * @return A [List] of all [Operation]s from all paths, or an empty list if none are defined.
+ *
+ * @author James Missen
+ *
+ * @see Operation
+ */
+val OpenAPI.operations
+    get() = PathItem::class.functions
+        .filter { it.returnType.classifier == Operation::class }
+        .map { method -> pathItems.mapNotNull { method.call(it) as? Operation } }
+        .flatten()
