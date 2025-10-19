cbb08f8493357e885a4197731a76df4689217701
James Missen <88438228+jamesmissen@users.noreply.github.com>
2025-10-19 14:38:39 +1300

    Included last event ID header and stream reconnection for country SSE endpoint

--- api/src/main/kotlin/com/jamesmissen/sse/api/controller/CountriesController.kt
+++ api/src/main/kotlin/com/jamesmissen/sse/api/controller/CountriesController.kt
@@ -2,6 +2,8 @@ package com.jamesmissen.sse.api.controller
 
 import com.jamesmissen.sse.api.model.Country
 import com.jamesmissen.sse.api.util.Constants.DATA_RESOURCE_LOCATION
+import com.jamesmissen.sse.api.util.extensions.decodeId
+import com.jamesmissen.sse.api.util.extensions.encodeId
 import com.jamesmissen.sse.api.util.extensions.readValue
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -11,6 +13,7 @@ import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE
 import org.springframework.http.codec.ServerSentEvent
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import tools.jackson.databind.json.JsonMapper
@@ -19,7 +22,7 @@ import java.time.Duration.ofMillis
 import java.time.Instant.ofEpochMilli
 import java.time.format.DateTimeFormatter
 import kotlin.io.encoding.Base64
-import kotlin.text.Charsets.UTF_8
+import kotlin.math.max
 
 /**
  * A REST controller for country resources.
@@ -62,6 +65,14 @@ class CountriesController(
          * @author James Missen
          */
         private const val RETRY = 10000L // milliseconds
+
+        /**
+         * A constant for the maximum number of events to be re-emitted when resuming a stream using the `Last-Event-ID`
+         * header.
+         *
+         * @author James Missen
+         */
+        private const val MAX_LATEST_EVENTS = 1000L
     }
 
     /**
@@ -90,6 +101,8 @@ class CountriesController(
      *
      * This endpoint produces an event stream, with each event including a [Country] `data` payload and a [String] `id`.
      *
+     * @param lastEventId The optional `Last-Event-ID` header used for resuming a stream.
+     *
      * @return A [Flow] of [ServerSentEvent]s, each with a [Country] `data` payload.
      *
      * @author James Missen
@@ -97,8 +110,12 @@ class CountriesController(
      * @see ServerSentEvent
      */
     @GetMapping("/stream", produces = [TEXT_EVENT_STREAM_VALUE])
-    final fun getCountriesStream() = flow {
-        val initialId = currentTimeMillis() / DELAY + 1
+    final fun getCountriesStream(@RequestHeader("Last-Event-ID") lastEventId: String?) = flow {
+        val currentId = currentTimeMillis() / DELAY + 1
+        val initialId = lastEventId
+            ?.let { id -> base64.decodeId(id, EVENT)?.plus(1) }
+            ?.let { id -> max(id, currentId - MAX_LATEST_EVENTS) }
+            ?: currentId
 
         generateSequence(initialId) { it + 1 }.forEach { id ->
             // Wait for next event time if necessary
@@ -110,7 +127,7 @@ class CountriesController(
             // Construct event
             val event = ServerSentEvent.builder<Country>()
                 .data(country)
-                .id(base64.encode("$EVENT-$id".toByteArray(UTF_8)))
+                .id(base64.encodeId(id, EVENT))
                 .event(EVENT)
                 .retry(ofMillis(RETRY))
                 .comment(dateTimeFormatter.format(timestamp))
