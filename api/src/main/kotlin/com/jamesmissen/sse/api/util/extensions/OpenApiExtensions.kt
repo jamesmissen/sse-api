7b69a54084c3f9b7ddbd7a56016d1898ec3f2f05
James Missen <88438228+jamesmissen@users.noreply.github.com>
2025-10-19 18:58:17 +1300

    Added custom configurations for SSE compatible with OpenAPI version 3.2

--- api/src/main/kotlin/com/jamesmissen/sse/api/util/extensions/OpenApiExtensions.kt
+++ api/src/main/kotlin/com/jamesmissen/sse/api/util/extensions/OpenApiExtensions.kt
@@ -1,8 +1,11 @@
 package com.jamesmissen.sse.api.util.extensions
 
+import io.swagger.v3.core.converter.ModelConverters
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.Operation
 import io.swagger.v3.oas.models.PathItem
+import io.swagger.v3.oas.models.responses.ApiResponse
+import kotlin.reflect.KClass
 import kotlin.reflect.full.functions
 
 /**
@@ -42,3 +45,27 @@ val OpenAPI.operations
         .filter { it.returnType.classifier == Operation::class }
         .map { method -> pathItems.mapNotNull { method.call(it) as? Operation } }
         .flatten()
+
+/**
+ * Returns all response elements defined in this API definition.
+ *
+ * Each item is an [ApiResponse] instance corresponding to a single API endpoint response.
+ *
+ * @receiver The [OpenAPI] instance to extract the [ApiResponse]s from.
+ *
+ * @return A [List] of all [ApiResponse]s from all paths and operations, or an empty list if none are defined.
+ *
+ * @author James Missen
+ *
+ * @see ApiResponse
+ */
+val OpenAPI.responses
+    get() = operations
+        .mapNotNull { it.responses }
+        .flatMap { it.values }
+
+fun Class<*>.toSchema() = ModelConverters.getInstance().read(this)
+    .values
+    .firstOrNull()
+
+fun KClass<*>.toSchema() = this.java.toSchema()
